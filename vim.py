38fd6be3e032f12b766ded26d2e0259d643a04bc
ancodeb <141550847+ancodeb@users.noreply.github.com>
2023-10-01 13:59:59 +0800

    Add files via upload

---
+++ vim.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import webbrowser
+
+import keyboard
+
+os.system("cls")
+print("Help can read help.\nThis is vim 0.0.2.\nBy ancodeb.\nGithub:https://www.github.com/ancodeb.\nChina gov can type cg\ngithub can go to github")
+print("happy china's day!")
+print("""
+  _     _   _                   ____                                 _ _   _           _                             __                       _      _     
+ | |   | | | |            _    / / /                                (_) | | |         | |                           / /                      | |    | |    
+ | |__ | |_| |_ _ __  ___(_)  / / /_      ____      ____      ____ _ _| |_| |__  _   _| |__   ___ ___  _ __ ___    / /_ _ _ __   ___ ___   __| | ___| |__  
+ | '_ \| __| __| '_ \/ __|   / / /\ \ /\ / /\ \ /\ / /\ \ /\ / / _` | | __| '_ \| | | | '_ \ / __/ _ \| '_ ` _ \  / / _` | '_ \ / __/ _ \ / _` |/ _ \ '_ \ 
+ | | | | |_| |_| |_) \__ \_ / / /  \ V  V /  \ V  V /  \ V  V / (_| | | |_| | | | |_| | |_) | (_| (_) | | | | | |/ / (_| | | | | (_| (_) | (_| |  __/ |_) |
+ |_| |_|\__|\__| .__/|___(_)_/_/    \_/\_/    \_/\_/    \_/\_(_)__, |_|\__|_| |_|\__,_|_.__(_)___\___/|_| |_| |_/_/ \__,_|_| |_|\___\___/ \__,_|\___|_.__/ 
+               | |                                              __/ |                                                                                      
+               |_|                                             |___/                                                                                       
+""")
+while True:
+    a=input(":")
+    if a=="help":
+        print("cg can find china gov.\nhelp can read help.\nq can quit vim.\nql can exit help.\ncreat is new file.\nqiuhuvim can close files.")
+    elif a=="cg":
+        print("china gov is china gov.\nit is https://www.gov.cn")
+    elif a=="q":
+        os.system("cls")
+        sys.exit()
+    elif a=="ql":
+        os.system("cls")
+    elif a=="creat":
+        hello=True
+        os.system("cls")
+        ti=[]
+        while hello:
+            if keyboard.is_pressed('i'):
+                text=input(":")
+                ti.append(text)
+                os.system("cls")
+                for i in ti:
+                    print(i)
+            if keyboard.is_pressed('q'):
+                break
+    elif a=='happy':
+        webbrowser.open('https://www.gov.cn/')
+    elif a=='github':
+        webbrowser.open('https://www.github.com/ancodeb')
+
+
+
+    else:
+        print("erro")
